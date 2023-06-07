ad12b4bd5adcb0da4c8e2bbefc102d1238856e28
Muhammad Anis Naseem <muhammadanis16734@gmail.com>
2023-06-07 11:42:00 +0500

    Comment out the Consumer Class

--- src/main/java/com/example/springbootjdbcconnection/controller/CustomerHandlerConsumer.java
+++ src/main/java/com/example/springbootjdbcconnection/controller/CustomerHandlerConsumer.java
@@ -1,26 +1,26 @@
-package com.example.springbootjdbcconnection.controller;
-
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-
-@Component
-@RabbitListener
-public class CustomerHandlerConsumer {
-
-
-    @RabbitHandler
-    @RabbitListener(queues = "jdbcTest.Queue")
-    public void recieveMessageFromQueue(String smsConsumer) {
-
-            LocalDateTime now = LocalDateTime.now();
-            System.out.println("Message is {}" + smsConsumer + now);
-
-    }
-
-
-    }
-
+//package com.example.springbootjdbcconnection.controller;
+//
+//import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDateTime;
+//
+//@Component
+//@RabbitListener
+//public class CustomerHandlerConsumer {
+//
+//
+//    @RabbitHandler
+//    @RabbitListener(queues = "jdbcTest.Queue")
+//    public void recieveMessageFromQueue(String smsConsumer) {
+//
+//            LocalDateTime now = LocalDateTime.now();
+//            System.out.println("Message is {}" + smsConsumer + now);
+//
+//    }
+//
+//
+//    }
+//
